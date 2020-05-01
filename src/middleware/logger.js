ba4ba2ed35893f9abe9bb1e06a40c18f6f5f010c
sokcuri <sokcuri@gmail.com>
2020-05-01 10:11:48 +0900

    remove duplicated item

--- src/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var loggerMiddleware = function (req, resp, next) {
-    console.log('Request logged:', req.method, req.path);
-    next();
-};
-exports.default = loggerMiddleware;
