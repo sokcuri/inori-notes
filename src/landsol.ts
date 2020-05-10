ccc9b3326f596586207ab0e21feaff9b75258de0
sokcuri <sokcuri@gmail.com>
2020-05-10 15:51:26 +0900

    set default object for typeorm helper

--- src/landsol.ts
+++ src/landsol.ts
@@ -1,13 +1,13 @@
 import { getRepository, ObjectType } from 'typeorm';
 
-export async function getOne<Entity>(entity: ObjectType<Entity>, param: object): Promise<Entity> {
+export async function getOne<Entity>(entity: ObjectType<Entity>, param: object = {}): Promise<Entity> {
   return await getRepository<Entity>(entity)
   .createQueryBuilder('base')
   .where(Object.keys(param).map(x => `base.${x} = :${x}`).join(' AND '), param)
   .getOne();
 }
 
-export async function getMany<Entity>(entity: ObjectType<Entity>, param: object): Promise<Entity[]> {
+export async function getMany<Entity>(entity: ObjectType<Entity>, param: object = {}): Promise<Entity[]> {
   return await getRepository<Entity>(entity)
   .createQueryBuilder('base')
   .where(Object.keys(param).map(x => `base.${x} = :${x}`).join(' AND '), param)
